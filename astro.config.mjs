20db02cee5c36699a78f51106fd0e4f80d2c40b3
SvmurviWolf <defors3@gmail.com>
2024-07-05 15:39:44 +0300

    ADDED: analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     output: 'server',
     adapter: vercel({
         webAnalytics: { enabled: true }
     }),
+    vite: {
+        ssr: {
+            noExternal: ['path-to-regexp'],
+        },
+    },
 });
