9695e862932618ae9acd2558ea775300f4b1721b
SvmurviWolf <defors3@gmail.com>
2024-07-26 17:07:43 +0300

    CHANGED: github auth link

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,2 +1,2 @@
 export const apiURL = 'https://snake-ranking-be.fly.dev';
-export const getGithubAuthLink = (path: string) =>  `https://github.com/login/oauth/authorize?scope=user:email&client_id=Ov23liOZXYzKWI9W2YDL&redirect_uri=https://3310snake.com/${path}`;
+export const getGithubAuthLink = (path: string) =>  `https://github.com/login/oauth/authorize?scope=user:email&client_id=Ov23litd36zZDLBBuTZM&redirect_uri=https://3310snake.com/${path}`;
